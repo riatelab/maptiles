6a75af6a41cccd130dd0652fb54f165ffc880cf5
rCarto <timothee.giraud@cnrs.fr>
2022-08-02 10:24:11 +0200

    test: update coverage file

--- inst/tinytest/test_get_tiles.R
+++ inst/tinytest/test_get_tiles.R
@@ -1,5 +1,5 @@
 
-home <- length(unclass(packageVersion("maptiles"))[[1]]) == 4
+home <- length(unclass(packageVersion("maptiles"))[[1]]) == 3
 if(home){
   suppressPackageStartupMessages(library(sf))
   suppressPackageStartupMessages(library(terra))
