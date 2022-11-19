ce636747e1e59ab23f4749648ea3e93f33c77b35
rCarto <timothee.giraud@cnrs.fr>
2022-11-19 19:07:45 +0100

    fix: remove gdal version check
    it was a very old version

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  gdv <- terra::gdal()
-  if (gdv < "2.2.3") {
-    a <- paste("\nNOTE: using GDAL version", gdv,
-               "\nYou need at least version 2.2.3\n")
-    packageStartupMessage(a)
-  }
-}
