24f1024b8b3294fa1c011c8fb03afb097addc021
rCarto <timothee.giraud@cnrs.fr>
2023-04-03 11:40:57 +0200

    test: add tests for actual calls to providers

--- inst/tinytest/test_get_tiles.R
+++ inst/tinytest/test_get_tiles.R
@@ -1,9 +1,8 @@
 home <- length(unclass(packageVersion("maptiles"))[[1]]) == 4
 
-library(sf)
-library(terra)
+suppressPackageStartupMessages(library(sf))
+suppressPackageStartupMessages(library(terra))
 nc <- st_read(system.file("shape/nc.shp", package="sf"), quiet = TRUE)
-# nc_sf <- st_transform(nc, 2153)
 nc_sf <- nc
 nc_sfc <- st_geometry(nc_sf)
 nc_bbox <- st_bbox(nc_sf)
@@ -14,6 +13,8 @@ nc_sf_centro <- nc_sf[1, ]
 st_geometry(nc_sf_centro) <- st_centroid(st_geometry(nc_sf_centro))
 nc_SpatVector_centro <- vect(nc_sf_centro)
 
+
+# test allowed and forbidden input
 expect_silent(maptiles:::test_input(nc_sf))
 expect_silent(maptiles:::test_input(nc_sfc))
 expect_silent(maptiles:::test_input(nc_bbox))
@@ -24,8 +25,8 @@ expect_silent(maptiles:::test_input(nc_SpatVector_centro))
 expect_silent(maptiles:::test_input(nc_sf_centro))
 expect_error(maptiles:::test_input("bop"))
 
-
-input = nc_sf
+# test proj mgmgt
+input <- nc_sf
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -33,7 +34,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_sfc
+input <- nc_sfc
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -41,7 +42,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_bbox
+input <- nc_bbox
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -49,7 +50,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_SpatVector
+input <- nc_SpatVector
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -57,7 +58,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_SpatRaster
+input <- nc_SpatRaster
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -65,7 +66,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_SpatExtent
+input <- nc_SpatExtent
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs("epsg:4326"))
 expect_true(st_crs(x[[2]]) == st_crs("epsg:4326"))
@@ -73,7 +74,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_sf_centro
+input <- nc_sf_centro
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -81,7 +82,7 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-input = nc_SpatVector_centro
+input <- nc_SpatVector_centro
 x <- maptiles:::get_bbox_and_proj(input)
 expect_true(st_crs(x[[1]]) == st_crs(input))
 expect_true(st_crs(x[[2]]) == st_crs(input))
@@ -89,73 +90,112 @@ expect_true(st_crs(x[[3]]) == st_crs("epsg:4326"))
 expect_inherits(x[[2]], "bbox")
 expect_inherits(x[[3]], "bbox")
 
-#
-# if(home){
-#   # test sf input
-#   expect_true(inherits(get_tiles(x = bb), "SpatRaster"))
-#   # test SpatExtent input
-#   expect_true(inherits(get_tiles(x = se), "SpatRaster"))
-#   # test SpatVector input
-#   a <- get_tiles(x = nc.sv)
-#   expect_true(inherits(a, "SpatRaster"))
-#   # test SpatRaster input
-#   expect_true(inherits(get_tiles(x = a), "SpatRaster"))
-#   # test verbosity
-#   suppressMessages(expect_message(get_tiles(x = nc, verbose=TRUE)))
-#   # test crop
-#   expect_true(inherits(get_tiles(x = nc, crop = TRUE), "SpatRaster"))
-#   # test 1 point
-#   expect_true(inherits(get_tiles(x = nc1, crop = TRUE), "SpatRaster"))
-#   # test zoom and provider
-#   expect_true(inherits(get_tiles(x = nc[1:2,],
-#                                     provider = "Stamen.Watercolor",
-#                                     zoom = 2), "SpatRaster"))
-#   # test custom server
-#   fullserver = paste("https://server.arcgisonline.com/ArcGIS/rest/services",
-#                      "Specialty/DeLorme_World_Base_Map/MapServer",
-#                      "tile/{z}/{y}/{x}.jpg", sep = "/")
-#   esri <-  list(src = 'esri', q = fullserver, sub = NA,
-#                 cit = 'Tiles: Esri; Copyright: 2012 DeLorme')
-#   expect_true(inherits(get_tiles(x = nc, provider = esri, crop = TRUE,
-#                                     verbose = FALSE), "SpatRaster"))
-#   # test cachedir
-#   expect_true(inherits(get_tiles(x = nc, cachedir = tempdir(),
-#                                     forceDownload = TRUE, zoom = 2),
-#                           "SpatRaster"))
-#   # test garbage input
-#   expect_error(get_tiles(x = "1", zoom = 1))
-#   # test plot
-#   x <- get_tiles(x = nc)
-#   expect_silent(plot_tiles(x, add = FALSE))
-#   expect_silent(plot_tiles(x, add = TRUE))
-#   expect_silent(plot_tiles(x, adjust = TRUE))
-#   # test credits
-#   expect_silent(get_credit("OpenStreetMap"))
-# }
-#
-#
-#
-#
-# # library(sf)
-# # library(terra)
-# # nc <- st_read(system.file("shape/nc.shp", package="sf"), quiet = TRUE)
-# # nc_sf <- st_transform(nc, 3857)
-# # nc_sfc <- st_geometry(nc_sf)
-# # nc_bbox <- st_bbox(nc_sf)
-# # nc_SpatVector <- vect(nc_sf)
-# # nc_SpatRaster <- rast(nc_SpatVector)
-# # nc_SpatExtent <- ext(project(nc_SpatVector, "epsg:4326"))
-# # nc_sf_centro <- nc_sf[1, ]
-# # st_geometry(nc_sf_centro) <- st_centroid(st_geometry(nc_sf_centro))
-# # nc_SpatVector_centro <- vect(nc_sf_centro)
-# #
-# # expect_silent(test_input(nc_sf))
-# # expect_silent(test_input(nc_sfc))
-# # expect_silent(test_input(nc_bbox))
-# # expect_silent(test_input(nc_SpatVector))
-# # expect_silent(test_input(nc_SpatRaster))
-# # expect_silent(test_input(nc_SpatExtent))
-# # expect_silent(test_input(nc_SpatVector_centro))
-# # expect_silent(test_input(nc_sf_centro))
-# # expect_error(test_input("bop"))
-# #
+
+
+if(home) {
+  # test full fun
+  input <- nc_sf
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_sfc
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_bbox
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_SpatVector
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_SpatRaster
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_SpatExtent
+  x <- get_tiles(input)
+  expect_true(crs(x) == crs("epsg:4326"))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_sf_centro
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+  input <- nc_SpatVector_centro
+  x <- get_tiles(input)
+  expect_true(st_crs(x) == st_crs(input))
+  expect_inherits(x, "SpatRaster")
+
+  # test project
+  input <- nc_sf
+  x <- get_tiles(x = input, project = FALSE)
+  expect_equal(crs(x), crs("epsg:3857"))
+
+  # test verbosity
+  input <- nc_sf
+  suppressMessages(expect_message(get_tiles(x = input, verbose=TRUE)))
+
+  # test crop
+  input <- nc_sf
+  x <- get_tiles(x = input, crop = TRUE)
+  expect_inherits(x, "SpatRaster")
+  expect_equivalent(st_bbox(input), st_bbox(x), tolerance = 0.001)
+
+  # test zoom and provider
+  input <- nc_sf
+  x <- get_tiles(x = input,
+                 provider = "Stamen.Watercolor",
+                 zoom = 2)
+  expect_inherits(x, "SpatRaster")
+
+  # test custom server
+  fullserver = paste("https://server.arcgisonline.com/ArcGIS/rest/services",
+                     "Specialty/DeLorme_World_Base_Map/MapServer",
+                     "tile/{z}/{y}/{x}.jpg", sep = "/")
+  esri <- list(src = 'esri', q = fullserver, sub = NA,
+               cit = 'Tiles: Esri; Copyright: 2012 DeLorme')
+  input <- nc_sf
+  x <- get_tiles(x = input, provider = esri, crop = TRUE, verbose = FALSE)
+  expect_inherits(x, "SpatRaster")
+
+
+  # test error custom server
+  fullserver = paste("https://server.arcgisonline.com/ArcGIS/rest/servixces",
+                     "Specialty/DeLorme_World_Base_Map/MapServer",
+                     "tile/{z}/{y}/{x}.jpg", sep = "/")
+  esrix <- list(src = 'esrix', q = fullserver, sub = NA,
+               cit = 'Tiles: Esri; Copyright: 2012 DeLorme')
+  input <- nc_sf
+  expect_message(get_tiles(x = input, provider = esrix, crop = TRUE, verbose = FALSE))
+
+
+  # test cachedir
+  input <- nc_sf
+  cache_d <- tempdir()
+  x <- get_tiles(x = input, cachedir = tempdir(),
+                 forceDownload = FALSE, zoom = 2)
+  expect_true(file.exists(cache_d))
+  expect_inherits(x, "SpatRaster")
+
+  # test forceDownload
+  input <- nc_sf
+  x <- get_tiles(x = input, cachedir = tempdir(),
+                 forceDownload = TRUE, zoom = 2)
+  expect_inherits(x, "SpatRaster")
+
+
+
+  # test plot
+  input <- nc_sf
+  x <- get_tiles(x = input, crop = TRUE)
+  expect_message(plot_tiles(NULL))
+  expect_warning(plot_tiles(input))
+  expect_silent(plot_tiles(x, add = FALSE))
+  expect_silent(plot_tiles(x, add = TRUE))
+  expect_silent(plot_tiles(x, adjust = TRUE))
+
+
+  # test credits
+  expect_equal(get_credit("OpenStreetMap"), "© OpenStreetMap contributors")
+}
